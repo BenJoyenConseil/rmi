8ba310eeadcd7bf735081db6a788c008f78982c9
BenJoyenConseil <b.joyenconseil@gmail.com>
2020-11-10 03:19:51 +0100

    WIP : index lookup

---
+++ linear/index.go
@@ -0,0 +1,45 @@
+package linear
+
+import (
+	"math"
+)
+
+type LearnedIndex struct {
+	m        *RegressionModel
+	len      int
+	maxError int
+}
+
+/*
+New return an Index fitted and data
+*/
+func New(dataset []float64) *LearnedIndex {
+	x, y := cdf(dataset)
+	len := len(dataset)
+	m := Fit(x, y)
+	maxErr := 0
+	for i, k := range x {
+		guessPosition := math.Round(m.Predict(k)*float64(len) - 1)
+		truePosition := y[i]*float64(len) - 1
+		residual := math.Sqrt(math.Pow(truePosition-guessPosition, 2))
+		if float64(maxErr) < residual {
+			maxErr = int(residual)
+		}
+	}
+	return &LearnedIndex{m: m, len: len, maxError: maxErr}
+}
+
+/*
+GuessIndex return the predicted position of the key in the index
+and upper / lower positions' search interval
+*/
+func (idx *LearnedIndex) GuessIndex(key float64) (guess, lower, upper int) {
+	guess = int(math.Round(idx.m.Predict(key)*float64(idx.len) - 1))
+	lower = guess - idx.maxError
+	upper = guess + idx.maxError
+	return
+}
+
+func (idx *LearnedIndex) Lookup(key float64) (position int) {
+	return -1
+}
---
+++ linear/index_test.go
@@ -0,0 +1,42 @@
+package linear
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	keys := []float64{5, 3, 3, 3.14, 10, 2.5, 2.98}
+
+	// when
+	idx := New(keys)
+
+	// then
+	assert.Equal(t, 7, idx.len)
+	assert.Equal(t, .23119036646681634, idx.m.intercept)
+	assert.Equal(t, .08523040437506509, idx.m.slope)
+	assert.Equal(t, 2, idx.maxError)
+}
+
+func TestGuessIndex(t *testing.T) {
+	idx := &LearnedIndex{
+		m:   &RegressionModel{intercept: .23119036646681634, slope: .08523040437506509},
+		len: 7,
+	}
+
+	// when
+	guess, lower, upper := idx.GuessIndex(1.)
+
+	// then
+	assert.Equal(t, 2, guess)
+	assert.Equal(t, 0, lower)
+	assert.Equal(t, 4, upper)
+}
+
+func TestLookup(t *testing.T) {
+	// when
+	idx := &LearnedIndex{}
+	// when
+	idx.Lookup(1.)
+}
