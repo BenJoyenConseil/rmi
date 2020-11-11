19ee63e34a23bb8c863d8626a8873a490ab13b00
BenJoyenConseil <b.joyenconseil@gmail.com>
2020-11-11 02:25:20 +0100

    add lookup to a learned index

--- linear/index.go
+++ linear/index.go
@@ -1,13 +1,21 @@
 package linear
 
 import (
+	"fmt"
+	"log"
 	"math"
 )
 
+type kv struct {
+	key    float64
+	offset int
+}
+
 type LearnedIndex struct {
-	m        *RegressionModel
-	len      int
-	maxError int
+	m           *RegressionModel
+	sortedTable []*kv
+	len         int
+	maxError    int
 }
 
 /*
@@ -19,8 +27,8 @@ func New(dataset []float64) *LearnedIndex {
 	m := Fit(x, y)
 	maxErr := 0
 	for i, k := range x {
-		guessPosition := math.Round(m.Predict(k)*float64(len) - 1)
-		truePosition := y[i]*float64(len) - 1
+		guessPosition := math.Round(scale(m.Predict(k), len))
+		truePosition := math.Round(scale(y[i], len))
 		residual := math.Sqrt(math.Pow(truePosition-guessPosition, 2))
 		if float64(maxErr) < residual {
 			maxErr = int(residual)
@@ -29,20 +37,70 @@ func New(dataset []float64) *LearnedIndex {
 	return &LearnedIndex{m: m, len: len, maxError: maxErr}
 }
 
+func scale(cdfVal float64, datasetLen int) float64 {
+	return cdfVal*float64(datasetLen) - 1
+}
+
 /*
 GuessIndex return the predicted position of the key in the index
 and upper / lower positions' search interval
 */
 func (idx *LearnedIndex) GuessIndex(key float64) (guess, lower, upper int) {
-	cdfPred := idx.m.Predict(key)
-	scaleCDF := cdfPred*float64(idx.len) - 1
-	roundPosition := math.Round(scaleCDF)
-	guess = int(roundPosition)
+	guess = int(math.Round(scale(idx.m.Predict(key), idx.len)))
+	if guess < 0 {
+		guess = 0
+	} else if guess > idx.len-1 {
+		guess = idx.len - 1
+	}
 	lower = guess - idx.maxError
+	if lower < 0 {
+		lower = 0
+	}
 	upper = guess + idx.maxError
+	if upper > idx.len-1 {
+		upper = idx.len - 1
+	}
 	return guess, lower, upper
 }
 
-func (idx *LearnedIndex) Lookup(key float64) (position int) {
-	return -1
+/*
+Lookup return the actual value or err if the key is not found in the index
+*/
+func (idx *LearnedIndex) Lookup(key float64) (offset int, err error) {
+	guess, lower, upper := idx.GuessIndex(key)
+
+	if 0 <= guess && guess < idx.len {
+		if idx.sortedTable[guess].key == key {
+			return idx.sortedTable[guess].offset, nil
+		} else if idx.sortedTable[guess].key < key {
+			return binarySearch(key, idx.sortedTable[guess+1:upper+1])
+		} else {
+			return binarySearch(key, idx.sortedTable[lower:guess])
+		}
+	}
+
+	return -1, fmt.Errorf("The following key <%f> is not found in the index", key)
+}
+
+/*
+binarySearch implementation is for finding the leftmost element
+*/
+func binarySearch(key float64, searchSpace []*kv) (offeset int, err error) {
+	L := 0
+	R := len(searchSpace) - 1
+	nIter := 0
+
+	for L <= R {
+		m := int(math.Floor(float64((L + R) / 2)))
+		if searchSpace[m].key < key {
+			L = m + 1
+		} else if searchSpace[m].key > key {
+			R = m - 1
+		} else {
+			return searchSpace[m].offset, nil
+		}
+		nIter++
+	}
+	log.Println(nIter)
+	return -1, fmt.Errorf("The following key <%f> is not found in the index", key)
 }
--- linear/index_test.go
+++ linear/index_test.go
@@ -28,17 +28,115 @@ func TestGuessIndex(t *testing.T) {
 
 	// when
 	guess, lower, upper := idx.GuessIndex(2.95)
-
-	// then
 	assert.Equal(t, 2, guess)
 	assert.Equal(t, 0, lower)
 	assert.Equal(t, 4, upper)
+
+	// when guess < 0 ==> -1
+	guess, lower, upper = idx.GuessIndex(-2.95)
+	assert.Equal(t, 0, guess)
+	assert.Equal(t, 0, lower)
+	assert.Equal(t, 2, upper)
+
+	// when guess > len-1 ==> 7
+	guess, lower, upper = idx.GuessIndex(10.)
+	assert.Equal(t, 6, guess)
+	assert.Equal(t, 4, lower)
+	assert.Equal(t, 6, upper)
 }
 
 func TestLookup(t *testing.T) {
+	// given
+	idx := &LearnedIndex{
+		m:        &RegressionModel{intercept: .23119036646681634, slope: .08523040437506509},
+		len:      7,
+		maxError: 2,
+		//	keys : {5, 3, 3, 3.14, 10, 2.5, 2.98}
+		//  sort : {2.5, 2.98, 3, 3, 3.14, 5, 10}
+		//  posi : {0,   1,    2, 3, 4,    5, 6}
+		sortedTable: []*kv{
+			&kv{key: 2.5, offset: 5},
+			&kv{key: 2.98, offset: 6},
+			&kv{key: 3., offset: 1},
+			&kv{key: 3., offset: 2},
+			&kv{key: 3.14, offset: 3},
+			&kv{key: 5., offset: 0},
+			&kv{key: 10., offset: 4},
+		},
+	}
+
 	// when
-	idx := &LearnedIndex{}
-	
+	offset, err := idx.Lookup(2.98)
+	assert.Nil(t, err)
+	assert.Equal(t, 6, offset)
+
 	// when
-	idx.Lookup(1.)
+	offset, err = idx.Lookup(3.)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, offset)
+
+	// when
+	offset, err = idx.Lookup(3.14)
+	assert.Nil(t, err)
+	assert.Equal(t, 3, offset)
+
+	// when
+	offset, err = idx.Lookup(10.)
+	assert.Nil(t, err)
+	assert.Equal(t, 4, offset)
+
+	// when not in the index
+	offset, err = idx.Lookup(199.)
+	assert.NotNil(t, err)
+	assert.Equal(t, -1, offset)
+}
+
+func TestBinarySearch(t *testing.T) {
+	// given
+	kspace := []*kv{
+		&kv{key: 1, offset: 0},
+		&kv{key: 1, offset: 1},
+		&kv{key: 1, offset: 2},
+		&kv{key: 3, offset: 3},
+		&kv{key: 3.14, offset: 4},
+		&kv{key: 5, offset: 123},
+		&kv{key: 5, offset: 124},
+		&kv{key: 7.544, offset: 125},
+		&kv{key: 7.544, offset: 126},
+		&kv{key: 7.599, offset: 127},
+	}
+
+	// when
+	offset, err := binarySearch(1, kspace)
+	assert.Contains(t, []int{0, 1, 2}, offset)
+	assert.Nil(t, err)
+
+	// when
+	offset, err = binarySearch(3, kspace)
+	assert.Equal(t, 3, offset)
+	assert.Nil(t, err)
+
+	// when
+	offset, err = binarySearch(3.14, kspace)
+	assert.Equal(t, 4, offset)
+	assert.Nil(t, err)
+
+	// when
+	offset, err = binarySearch(5, kspace)
+	assert.Contains(t, []int{123, 124}, offset)
+	assert.Nil(t, err)
+
+	// when
+	offset, err = binarySearch(7.544, kspace)
+	assert.Equal(t, 125, offset)
+	assert.Nil(t, err)
+
+	// when
+	offset, err = binarySearch(7.599, kspace)
+	assert.Equal(t, 127, offset)
+	assert.Nil(t, err)
+
+	// when not found
+	offset, err = binarySearch(14, kspace)
+	assert.NotNil(t, err)
 }
